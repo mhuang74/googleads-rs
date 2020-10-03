df112a478e10eba2997c69bc93c5d35efe78e5be
Konosuke Kachi <github@s.k-kachi.net>
2020-10-03 19:17:44 +0900

    Remove optional labels from proto files

--- utils/update.sh
+++ utils/update.sh
@@ -6,4 +6,5 @@ wget https://github.com/googleapis/googleapis/archive/master.zip && unzip -q mas
 mv googleapis-master/google proto
 mv googleapis-master/grafeas proto
 find proto -type f -not -name '*.proto' -delete
+find proto -type f | xargs sed -i'' -e 's/^ *optional//g'
 rm -rf googleapis-master master.zip
